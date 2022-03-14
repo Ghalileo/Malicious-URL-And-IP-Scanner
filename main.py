3fc08a88c852ad8059df0d24d58070d1ba39ca17
Ghalileo <ose.okogbo@gmail.com>
2022-03-14 13:20:38 -0500

    API working as intended.  Separates malicious from none malicious scans

--- main.py
+++ main.py
@@ -13,13 +13,13 @@ url = 'https://www.virustotal.com/vtapi/v2/url/report'
 
 for i in Urls:
     parameters = {'apikey': API_key, 'resource': i}
-    response = request.get(url=url, params=parameters)
-    json_response= json.loads(response.text)
-    if json_response['response'] <= 0:
+    response = requests.get(url=url, params=parameters)
+    json_response = json.loads(response.text)
+    if json_response['response_code'] <= 0:
         with open('Virustotal Clean results.txt', 'a') as clean:
             clean.write(i) and clean.write("\tNot Malicious\n")
     else:
         with open ('Virustotal Malicious result.txt', 'a') as malicious:
-            malicious.write(1) and malicious.write("\t malicious \n")
+            malicious.write(i) and malicious.write("\t malicious \n")
 
     time.sleep(20)
